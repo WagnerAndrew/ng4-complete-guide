d3c884bdff7e66a5733a8afa0f5aec3435c4e51a
WagnerAndrew <wagner.andrew.james@gmail.com>
2019-07-03 13:04:49 -0600

    pre service conversion - some already slashed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,20 +9,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
-    ),
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
-    )
-  ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,23 @@
+import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply another test',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    )
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
