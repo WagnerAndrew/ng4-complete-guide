ddabe24b57ba5e03a9ffd83cd6dda42b8d3ba712
WagnerAndrew <wagner.andrew.james@gmail.com>
2019-07-03 14:54:06 -0600

    before routing was added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,43 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+      'Pesto Pasta',
+      'A delicious dish, high in protein, fiber and vitamins.',
+      'http://res.publicdomainfiles.com/pdf_view/2/13494451211528.jpg',
+      [
+        new Ingredient('Basil', 2),
+        new Ingredient('Tomatoe', 4),
+        new Ingredient('Pasta', 1)
+      ]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is simply another test',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+      'Rainbow Pizza',
+      'A vegetarian delight.',
+      'https://live.staticflickr.com/2015/2269983942_96804244fb_b.jpg',
+      [
+        new Ingredient('Peppers', 2),
+        new Ingredient('Tomatoe Sauce', 1),
+        new Ingredient('Dough', 1)
+      ]
     )
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
